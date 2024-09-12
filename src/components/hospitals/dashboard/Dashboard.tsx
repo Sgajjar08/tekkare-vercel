11ddc7eb43dd5a9447485913d143ec625df1d28d
Sgajjar08 <saurabhgajjar134@gmail.com>
2024-09-12 10:55:40 +0200

    delete everything

--- src/components/hospitals/dashboard/Dashboard.tsx
+++
@@ -1,6 +0,0 @@
-import HospitalTable from '../table/HospitalsTable';
-import hospitalData from '../../../data/data_exemple1.json';
-
-export const HospitalDashboard = () => (
-  <HospitalTable hospitals={hospitalData} />
-);
--- src/pages/Hospitals.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { HospitalDashboard } from '../components/hospitals/dashboard/Dashboard';
-
-const Hospitals: React.FC = () => <HospitalDashboard />;
-
-export default Hospitals;
