3879f0b014066dd80ca51c9d8f794bf1ffb1beca
Sgajjar08 <saurabhgajjar134@gmail.com>
2024-09-12 11:08:30 +0200

    resolved merge conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './contexts/LanguageContext.tsx';
 import './i18n.ts';
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
->>>>>>> parent of d179b84 (tekkare-versel)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 )
