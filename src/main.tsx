af72d86ef79666e8ecbcb6e180b82c59ccdaf3e3
Saurabh Gajjar <33550988+Sgajjar08@users.noreply.github.com>
2024-09-12 10:14:06 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './contexts/LanguageContext.tsx';
-import './i18n';
+import './i18n.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
